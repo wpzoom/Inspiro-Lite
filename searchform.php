4b56412c59db1e1d1f6c1fad66462c7571f87ef6
vicolaspetru <vicolaspetru@gmail.com>
2020-12-01 16:20:26 +0200

    [WIP] Search form from slide nav

--- searchform.php
+++ searchform.php
@@ -17,5 +17,5 @@
 		<span class="screen-reader-text"><?php echo _x( 'Search for:', 'label', 'inspiro' ); ?></span>
 	</label>
 	<input type="search" id="<?php echo $unique_id; ?>" class="search-field" placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'placeholder', 'inspiro' ); ?>" value="<?php echo get_search_query(); ?>" name="s" />
-	<button type="submit" class="search-submit"><?php echo inspiro_get_theme_svg( 'search' ); ?><span class="screen-reader-text"><?php echo _x( 'Search', 'submit button', 'inspiro' ); ?></span></button>
+	<button type="submit" class="search-submit"><?php echo inspiro_get_theme_svg( 'search' ); ?><span><?php echo _x( 'Search', 'submit button', 'inspiro' ); ?></span></button>
 </form>
