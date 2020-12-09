1a1fb69c8abbe8fa5d947b21a3157e1004ae848f
vicolaspetru <vicolaspetru@gmail.com>
2020-12-09 14:49:23 +0200

    Fixed issue when previous post is null

--- single.php
+++ single.php
@@ -27,22 +27,25 @@ get_header(); ?>
 		endif;
 
 		$previous_post = get_previous_post();
-		$prev_image = wp_get_attachment_image_src( get_post_thumbnail_id( $previous_post->ID ), 'entry-cover' );
-		$previous_cover = '';
 
-		if ( $prev_image && isset( $prev_image[0] ) ) {
-			$previous_cover = '<div class="previous-cover" style="background-image: url(' . esc_url( $prev_image[0] ) . ')"></div>';
+		if ( $previous_post ) {
+			$prev_image = wp_get_attachment_image_src( get_post_thumbnail_id( $previous_post->ID ), 'entry-cover' );
+			$previous_cover = '';
 
-			echo '<div class="previous-post-cover">';
+			if ( $prev_image && isset( $prev_image[0] ) ) {
+				$previous_cover = '<div class="previous-cover" style="background-image: url(' . esc_url( $prev_image[0] ) . ')"></div>';
 
-			the_post_navigation(
-				array(
-					'prev_text' => '<div class="previous-info">' . $previous_cover . '<div class="previous-content"><span class="screen-reader-text">' . __( 'Previous Post', 'inspiro' ) . '</span><span aria-hidden="true" class="nav-subtitle">' . __( 'Previous Post', 'inspiro' ) . '</span> <span class="nav-title">%title</span></div></div>',
-					'next_text' => '',
-				)
-			);
+				echo '<div class="previous-post-cover">';
 
-			echo '</div><!-- .previous-post-cover -->';
+				the_post_navigation(
+					array(
+						'prev_text' => '<div class="previous-info">' . $previous_cover . '<div class="previous-content"><span class="screen-reader-text">' . __( 'Previous Post', 'inspiro' ) . '</span><span aria-hidden="true" class="nav-subtitle">' . __( 'Previous Post', 'inspiro' ) . '</span> <span class="nav-title">%title</span></div></div>',
+						'next_text' => '',
+					)
+				);
+
+				echo '</div><!-- .previous-post-cover -->';
+			}
 		}
 
 	endwhile; // End the loop.
