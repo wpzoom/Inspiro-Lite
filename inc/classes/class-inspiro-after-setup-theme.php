3ce268e26a3ac092bfff637ca450af9e11fe250c
vicolaspetru <vicolaspetru@gmail.com>
2021-04-05 14:38:18 +0300

    Included TGM Plugin Activation

--- inc/classes/class-inspiro-after-setup-theme.php
+++ inc/classes/class-inspiro-after-setup-theme.php
@@ -46,6 +46,7 @@ if ( ! class_exists( 'Inspiro_After_Setup_Theme' ) ) {
         public function __construct() {
             add_action( 'after_setup_theme', array( $this, 'theme_setup' ) );
             add_action( 'template_redirect', array( $this, 'theme_content_width' ), 0 );
+            add_action( 'tgmpa_register',    array( $this, 'register_required_plugins' ) );
         }
 
         /**
@@ -227,6 +228,87 @@ if ( ! class_exists( 'Inspiro_After_Setup_Theme' ) ) {
              * @param int $content_width Content width in pixels.
              */
             $GLOBALS['content_width'] = apply_filters( 'inspiro_content_width', $content_width );
+        }
+
+        /**
+         * Register the required plugins for this theme.
+         *
+         * In this example, we register five plugins:
+         * - one included with the TGMPA library
+         * - two from an external source, one from an arbitrary source, one from a GitHub repository
+         * - two from the .org repo, where one demonstrates the use of the `is_callable` argument
+         *
+         * The variables passed to the `tgmpa()` function should be:
+         * - an array of plugin arrays;
+         * - optionally a configuration array.
+         * If you are not changing anything in the configuration array, you can remove the array and remove the
+         * variable from the function call: `tgmpa( $plugins );`.
+         * In that case, the TGMPA default settings will be used.
+         *
+         * This function is hooked into `tgmpa_register`, which is fired on the WP `init` action on priority 10.
+         */
+        public function register_required_plugins() {
+            /*
+             * Array of plugin arrays. Required keys are name and slug.
+             * If the source is NOT from the .org repo, then source is also required.
+             */
+
+            $plugins = array(
+
+                // This is an example of the use of 'is_callable' functionality. A user could - for instance -
+                // have Beaver installed *or* Beaver Premium. The slug would in that last case be different, i.e.
+                // 'bb-plugin'.
+                // By setting 'is_callable' to either a function from that plugin or a class method
+                // `array( 'class', 'method' )` similar to how you hook in to actions and filters, TGMPA can still
+                // recognize the plugin as being installed.
+                array(
+                    'name'          => 'Beaver Builder',
+                    'slug'          => 'beaver-builder-lite-version',
+                    'required'      => false,
+                    'is_callable'   => array( 'FLBuilderAdmin', 'activate' )
+                ),
+
+                array(
+                    'name'      => 'WPZOOM Addons for Beaver Builder',
+                    'slug'      => 'wpzoom-addons-for-beaver-builder',
+                    'required'  => false,
+                ),
+
+                array(
+                    'name'      => 'Instagram Widget by WPZOOM',
+                    'slug'      => 'instagram-widget-by-wpzoom',
+                    'required'  => false,
+                ),
+
+                array(
+                    'name'      => 'Social Icons Widget by WPZOOM',
+                    'slug'      => 'social-icons-widget-by-wpzoom',
+                    'required'  => false,
+                ),
+
+            );
+
+            /*
+             * Array of configuration settings. Amend each line as needed.
+             *
+             * TGMPA will start providing localized text strings soon. If you already have translations of our standard
+             * strings available, please help us make TGMPA even better by giving us access to these translations or by
+             * sending in a pull-request with .po file(s) with the translations.
+             *
+             * Only uncomment the strings in the config array if you want to customize the strings.
+             */
+            $config = array(
+                'id'           => 'inspiro_wporg',         // Unique ID for hashing notices for multiple instances of TGMPA.
+                'default_path' => '',                      // Default absolute path to bundled plugins.
+                'menu'         => 'tgmpa-install-plugins', // Menu slug.
+                'has_notices'  => true,                    // Show admin notices or not.
+                'dismissable'  => true,                    // If false, a user cannot dismiss the nag message.
+                'dismiss_msg'  => '',                      // If 'dismissable' is false, this message will be output at top of nag.
+                'is_automatic' => false,                   // Automatically activate plugins after installation or not.
+                'message'      => '',                      // Message to output right before the plugins table.
+            );
+
+            tgmpa( $plugins, $config );
         }
     }
 
