838f50665bb784f341700d98037232a9492b2ce6
Pavel Ciorici <cioricip@gmail.com>
2023-04-03 14:26:34 +0200

    Update theme-info-page.php

--- inc/theme-info-page.php
+++ inc/theme-info-page.php
@@ -831,7 +831,7 @@ function inspiro_display_theme_page() {
                                 <a href="https://www.wpzoom.com/themes/inspiro-blocks/" title="Inspiro Premium" target="_blank"><img src="<?php echo esc_url( get_template_directory_uri() . '/assets/images/admin/inspiro-blocks.png' ); ?>" width="300" alt="<?php echo esc_attr__( 'Inspiro Premium', 'inspiro' ); ?>" /></a>
 
                                 <div class="wpz-onboard_content-side-section-button">
-                                    <a href="<?php echo esc_url( admin_url( 'theme-install.php?search=inspiro%2Bblocks' ) ); ?>" title="Inspiro Blocks" target="_blank" class="button"><?php esc_html_e( 'Install Now', 'inspiro' ); ?> &nbsp;<?xml version="1.0" ?><svg class="feather feather-download" fill="none" height="20" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" width="20" xmlns="http://www.w3.org/2000/svg"><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"/><polyline points="7 10 12 15 17 10"/><line x1="12" x2="12" y1="15" y2="3"/></svg></a>
+                                    <a href="<?php echo esc_url( admin_url( 'theme-install.php?search=inspiro%2Bblocks' ) ); ?>" title="Inspiro Blocks" target="_blank" class="button"><?php esc_html_e( 'Install Now', 'inspiro' ); ?> &nbsp;<svg class="feather feather-download" fill="none" height="20" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" width="20" xmlns="http://www.w3.org/2000/svg"><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"/><polyline points="7 10 12 15 17 10"/><line x1="12" x2="12" y1="15" y2="3"/></svg></a>
                                 </div>
 
                             </div>
