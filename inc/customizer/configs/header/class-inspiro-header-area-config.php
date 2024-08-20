7dd0a4f8cde12d168c7de3071caa9f1040cfcac2
Josanu Andrei <andrei@quadra.md>
2024-08-20 16:18:52 +0300

    Code format

--- inc/customizer/configs/header/class-inspiro-header-area-config.php
+++ inc/customizer/configs/header/class-inspiro-header-area-config.php
@@ -7,7 +7,7 @@
  * @since Inspiro 1.3.0
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
+if (!defined('ABSPATH')) {
 	exit;
 }
 
@@ -16,13 +16,15 @@ if ( ! defined( 'ABSPATH' ) ) {
  *
  * @since 1.3.0
  */
-class Inspiro_Header_Area_Config {
+class Inspiro_Header_Area_Config
+{
 
 	/**
 	 * Constructor
 	 */
-	public function __construct() {
-		add_action( 'customize_register', array( $this, 'register_configuration' ), 10 );
+	public function __construct()
+	{
+		add_action('customize_register', array($this, 'register_configuration'), 10);
 	}
 
 	/**
@@ -31,13 +33,14 @@ class Inspiro_Header_Area_Config {
 	 * @param WP_Customize_Manager $wp_customize instance of WP_Customize_Manager.
 	 * @return void
 	 */
-	public function register_configuration( $wp_customize ) {
+	public function register_configuration($wp_customize)
+	{
 
 		// Create sections
 		$wp_customize->add_section(
 			'header-area',
 			array(
-				'title'    => esc_html__( 'Header', 'inspiro' ),
+				'title' => esc_html__('Header', 'inspiro'),
 				'priority' => 70,
 			)
 		);
@@ -52,7 +55,7 @@ class Inspiro_Header_Area_Config {
 		$wp_customize->add_setting(
 			'header-menu-style',
 			array(
-				'default'           => 'wpz_menu_normal',
+				'default' => 'wpz_menu_normal',
 				'sanitize_callback' => 'sanitize_key'
 			)
 		);
@@ -62,13 +65,13 @@ class Inspiro_Header_Area_Config {
 				$wp_customize,
 				'accordion_section_ui_wrapper',
 				array(
-					'type'             => 'accordion-section-ui-wrapper',
-					'label'            =>  esc_html__( 'Header Layout', 'inspiro' ),
-					'settings'         => 'accordion_ui_wrapper',
-					'section'          => 'header-area',
-					'description'      => __( 'This is another test.' ),
-					'accordion'        => true,
-					'expanded'         => false,
+					'type' => 'accordion-section-ui-wrapper',
+					'label' => esc_html__('Header Layout', 'inspiro'),
+					'settings' => 'accordion_ui_wrapper',
+					'section' => 'header-area',
+					'description' => __('This is another test.'),
+					'accordion' => true,
+					'expanded' => false,
 					'controls_to_wrap' => 2,
 				)
 			)
@@ -80,24 +83,24 @@ class Inspiro_Header_Area_Config {
 				'header-menu-style',
 				array(
 //					'label'           => esc_html__( 'Header Layout', 'inspiro' ),
-					'description'     => esc_html__( 'Select the header layout. The hamburger icon appears on the desktop if the Sidebar has at least one widget. On mobile devices, the main menu is displayed in the sidebar.', 'inspiro' ),
-					'section'         => 'header-area',
-					'choices'     => array(
+					'description' => esc_html__('Select the header layout. The hamburger icon appears on the desktop if the Sidebar has at least one widget. On mobile devices, the main menu is displayed in the sidebar.', 'inspiro'),
+					'section' => 'header-area',
+					'choices' => array(
 						'wpz_menu_normal' => array(
-							'label' => esc_html__( 'Default Style', 'inspiro' ),
-							'url'   => '%sheader-default.png'
+							'label' => esc_html__('Default Style', 'inspiro'),
+							'url' => '%sheader-default.png'
 						),
-						'wpz_menu_left'    => array(
-							'label' => esc_html__( 'Menu on left side', 'inspiro' ),
-							'url'   => '%sheader-left-menu.png'
+						'wpz_menu_left' => array(
+							'label' => esc_html__('Menu on left side', 'inspiro'),
+							'url' => '%sheader-left-menu.png'
 						),
-						'wpz_menu_center'    => array(
-							'label' => esc_html__( 'Menu on center', 'inspiro' ),
-							'url'   => '%sheader-center-menu.png'
+						'wpz_menu_center' => array(
+							'label' => esc_html__('Menu on center', 'inspiro'),
+							'url' => '%sheader-center-menu.png'
 						),
-						'wpz_menu_hamburger'    => array(
-							'label' => esc_html__( 'Hidden menu', 'inspiro' ),
-							'url'   => '%sheader-hidden-menu.png'
+						'wpz_menu_hamburger' => array(
+							'label' => esc_html__('Hidden menu', 'inspiro'),
+							'url' => '%sheader-hidden-menu.png'
 						),
 					)
 				)
@@ -117,16 +120,16 @@ class Inspiro_Header_Area_Config {
 				$wp_customize,
 				'header-menu-pro-style',
 				array(
-					'label'           => esc_html__( 'PRO', 'inspiro' ),
-					'section'         => 'header-area',
-					'choices'     => array(
-						'wpz_menu_left_logo_center'    => array(
-							'label' => esc_html__( 'Left menu with Logo Center', 'inspiro' ),
-							'url'   => '%sheader-pro.png',
+					'label' => esc_html__('PRO', 'inspiro'),
+					'section' => 'header-area',
+					'choices' => array(
+						'wpz_menu_left_logo_center' => array(
+							'label' => esc_html__('Left menu with Logo Center', 'inspiro'),
+							'url' => '%sheader-pro.png',
 						),
-						'wpz_menu_center_logo_center'    => array(
-							'label' => esc_html__( 'Center menu with Logo Center', 'inspiro' ),
-							'url'   => '%sheader-pro-2.png',
+						'wpz_menu_center_logo_center' => array(
+							'label' => esc_html__('Center menu with Logo Center', 'inspiro'),
+							'url' => '%sheader-pro-2.png',
 						),
 					)
 				)
@@ -137,43 +140,43 @@ class Inspiro_Header_Area_Config {
 			'header-layout-type',
 			array(
 				'sanitize_callback' => 'sanitize_key',
-				'default'           => 'wpz_layout_narrow'
+				'default' => 'wpz_layout_narrow'
 			)
 		);
 
 		$wp_customize->add_control(
 			'header-layout-type',
 			array(
-				'label'   => esc_html__( 'Header Menu Width', 'inspiro' ),
-				'type'    => 'radio',
+				'label' => esc_html__('Header Menu Width', 'inspiro'),
+				'type' => 'radio',
 				'section' => 'header-area',
 				'choices' => array(
-					'wpz_layout_narrow' => esc_html__( 'Narrow', 'inspiro' ),
-					'wpz_layout_full'   => esc_html__( 'Full-width', 'inspiro' )
+					'wpz_layout_narrow' => esc_html__('Narrow', 'inspiro'),
+					'wpz_layout_full' => esc_html__('Full-width', 'inspiro')
 				),
 			)
 		);
 
-        $wp_customize->add_setting(
-            'cover-size',
-            array(
-                'sanitize_callback' => 'sanitize_key',
-                'default'           => 'cover_fixed_height'
-            )
-        );
+		$wp_customize->add_setting(
+			'cover-size',
+			array(
+				'sanitize_callback' => 'sanitize_key',
+				'default' => 'cover_fixed_height'
+			)
+		);
 
-        $wp_customize->add_control(
-            'cover-size',
-            array(
-                'label'   => esc_html__( 'Featured Image Height in Posts and Pages', 'inspiro' ),
-                'type'    => 'radio',
-                'section' => 'header-area',
-                'choices' => array(
-                    'cover_fixed_height' => esc_html__( 'Fixed height', 'inspiro' ),
-                    'cover_fullscreen'   => esc_html__( 'Fullscreen', 'inspiro' )
-                ),
-            )
-        );
+		$wp_customize->add_control(
+			'cover-size',
+			array(
+				'label' => esc_html__('Featured Image Height in Posts and Pages', 'inspiro'),
+				'type' => 'radio',
+				'section' => 'header-area',
+				'choices' => array(
+					'cover_fixed_height' => esc_html__('Fixed height', 'inspiro'),
+					'cover_fullscreen' => esc_html__('Fullscreen', 'inspiro')
+				),
+			)
+		);
 
 		$wp_customize->add_setting(
 			'header_title_subsection',
@@ -182,35 +185,37 @@ class Inspiro_Header_Area_Config {
 				'sanitize_callback' => 'sanitize_text_field',
 			)
 		);
+
 		$wp_customize->add_control(
 			new Inspiro_Customize_Title_Control(
 				$wp_customize,
 				'header_title_subsection',
 				array(
-					'label'   => esc_html__( 'Elements', 'inspiro' ),
+					'label' => esc_html__('Elements', 'inspiro'),
 					'section' => 'header-area',
 				)
 			)
 		);
 
-
 		$wp_customize->add_setting(
 			'header_search_show',
 			array(
-				'capability'        => 'edit_theme_options',
-				'default'           => true,
+				'capability' => 'edit_theme_options',
+				'default' => true,
 				'sanitize_callback' => 'inspiro_sanitize_checkbox',
 			)
 		);
+
 		$wp_customize->add_control(
 			'header_search_show',
 			array(
-				'type'    => 'checkbox',
+				'type' => 'checkbox',
 				'section' => 'header-area',
-				'label'   => esc_html__( 'Show Search Icon', 'inspiro' ),
-				'description' => esc_html__( 'Show search icon and search form in the header', 'inspiro' ),
+				'label' => esc_html__('Show Search Icon', 'inspiro'),
+				'description' => esc_html__('Show search icon and search form in the header', 'inspiro'),
 			)
 		);
 	}
 }
+
 new Inspiro_Header_Area_Config();
