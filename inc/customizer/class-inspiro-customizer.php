d618aee30ae3eb604a9474b79962dd9e444947a3
Josanu Andrei <andrei@quadra.md>
2024-08-23 16:11:32 +0300

    Return previous layout for Section Pro

--- inc/customizer/class-inspiro-customizer.php
+++ inc/customizer/class-inspiro-customizer.php
@@ -263,15 +263,15 @@ if ( ! class_exists( 'Inspiro_Customizer' ) ) {
 					$wp_customize,
 					'inspiro_upgrade_pro',
 					array(
-						'title'       => esc_html__( 'Inspiro PRO Features', 'inspiro' ),
-//						'title'       => esc_html__( 'Upgrade to Inspiro Premium', 'inspiro' ),
-//						 'description' => esc_html__( 'Unlock premium features: 7 Style Kits, Video Backgrounds, Portfolio Integration, Premium Support and much more...', 'inspiro' ),
-//						'pro_text'    => esc_html__( 'View Inspiro Premium', 'inspiro' ),
-						'pro_text'    => esc_html__( 'Learn More', 'inspiro' ),
+//						'title'       => esc_html__( 'Inspiro PRO Features', 'inspiro' ),
+						'title'       => esc_html__( 'Upgrade to Inspiro Premium', 'inspiro' ),
+						 'description' => esc_html__( 'Unlock premium features: 7 Style Kits, Video Backgrounds, Portfolio Integration, Premium Support and much more...', 'inspiro' ),
+						'pro_text'    => esc_html__( 'View Inspiro Premium', 'inspiro' ),
+//						'pro_text'    => esc_html__( 'Learn More', 'inspiro' ),
 						'pro_url'     => 'https://www.wpzoom.com/themes/inspiro/?utm_source=wpadmin&utm_medium=customizer&utm_campaign=bluebutton',
-						'demo_link_text'    => esc_html__( 'View Demos', 'inspiro' ),
-						'demo_link_url'     => '/wp-admin/themes.php?page=inspiro#demos',
-						'priority'    => 200,
+//						'demo_link_text'    => esc_html__( 'View Demos', 'inspiro' ),
+//						'demo_link_url'     => '/wp-admin/themes.php?page=inspiro#demos',
+						'priority'    => 10,
 					)
 				)
 			);
