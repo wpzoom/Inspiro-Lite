aebb7e5cb5c1deef037b981c7e9c491374f97edb
Josanu Andrei <andrei@quadra.md>
2024-09-16 11:43:35 +0300

    Add logo and setup item menus and pages.

--- inc/admin/pages/admin.php
+++ inc/admin/pages/admin.php
@@ -1,10 +1,5 @@
-<?php $wpz_theme_name = wp_get_theme( get_template() ); ?>
-<div class="clear"></div>
 <div id="zoomWrap">
 	<div id="zoomHead">
-		<script type="text/javascript">
-			//var wpzoom_ajax_url = '<?php //echo admin_url( 'admin-ajax.php' ); ?>';
-		</script>
 		<div id="zoomLoading">
 			<p><?php _e( 'Loading', 'wpzoom' ); ?></p>
 		</div>
@@ -15,60 +10,11 @@
 			<p><?php _e( 'Can\'t save options. Please contact <a href="https://wpzoom.com/support/">WPZOOM Support</a>.', 'wpzoom' ); ?></p>
 		</div>
 		<div id="zoomTheme">
-			<?php
-			$name       = 'Demo Content';
-			$xml_data   = get_demo_xml_data();
-			$has_access = false;
-			$link_href  = 'wpz_' . substr( md5( $name ), 0, 8 );
 
-			if ( $xml_data['remote']['response'] ) {
-				$has_access = true;
-			} elseif ( $xml_data['local']['response'] ) {
-				$has_access = true;
-			}
-			?>
 			<h5>
-				<?php //printf( __( 'ZOOM Framework <strong>%s</strong>', 'wpzoom' ), WPZOOM::$wpzoomVersion ); ?>
+				Test
 			</h5>
-			<h3><?php //echo $wpz_theme_name->get( 'Name' ); ?> <span><?php echo $wpz_theme_name->get( 'Version' ); ?></span></h3>
 		</div>
 	</div><!-- /#zoomHead -->
-
-	<div class="head_meta">
-		<div id="zoomInfo">
-
-		</div>
-
-	</div>
-
-	<div class="admin_main">
-		<div id="zoomNav">
-			<?php WPZOOM_Admin_Settings_Page::menu(); ?>
-			<div class="cleaner">&nbsp;</div>
-		</div><!-- end #zoomNav -->
-
-		<div class="tab_container">
-			<form id="zoomForm" method="post">
-				<?php WPZOOM_Admin_Settings_Page::content(); ?>
-
-				<input type="hidden" name="action" value="save" />
-				<input type="hidden" id="zoom-nonce" name="_ajax_nonce" value="<?php echo wp_create_nonce( 'wpzoom-ajax-save' ); ?>" />
-			</form>
-		</div><!-- end .tab_container -->
-		<div class="clear"></div>
-	</div> <!-- /.admin_main -->
-
-	<div class="zoomActionButtons">
-
-		<form id="zoomReset" method="post">
-			<p class="submit" />
-			<input name="reset" class="button-secondary" type="submit" value="<?php _e( 'Reset settings', 'wpzoom' ); ?>" />
-			<input type="hidden" name="action" value="reset" />
-			</p>
-		</form>
-
-	</div><!-- end of .zoomActionButtons -->
-
 </div><!-- end #zoomWrap -->
 
-<div class="clear"></div>
--- inc/classes/class-inspiro-custom-wp-admin-menu.php
+++ inc/classes/class-inspiro-custom-wp-admin-menu.php
@@ -13,9 +13,15 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 /**
  * Main PHP class for wp-admin options page
+ *
+ * Menu items:
+ *  - About Inspiro
+ *  - Customizer
+ *  - Upgrade
  */
 if ( ! class_exists( 'Inspiro_Custom_Wp_Admin_Menu' ) ) {
 	class Inspiro_Custom_Wp_Admin_Menu {
+
 		public function __construct() {
 			add_action( 'admin_menu', array( $this, 'register_admin_pages' ) );
 		}
@@ -24,32 +30,69 @@ if ( ! class_exists( 'Inspiro_Custom_Wp_Admin_Menu' ) ) {
 			require_once INSPIRO_THEME_DIR . 'inc/admin/pages/admin.php';
 		}
 
+		public function about() {
+			echo '<h1>About Inspiro</h1>';
+		}
+
+		public function upgrade() {
+			echo '<h1>Upgrade Options</h1>';
+		}
+
 		/**
 		 * Register custom menu for wp-admin.
 		 *
 		 * @return void
 		 */
 		public function register_admin_pages() {
+			// Add the main menu page
 			add_menu_page(
-				__( 'Page Title', 'wpzoom' ), // page title
-				__( 'Inspiro', 'inspiro' ),  // menu title
+				__( 'Page Title', 'wpzoom' ),   // page title
+				__( 'Inspiro', 'inspiro' ),     // menu title
 				'manage_options',
-				'inspiro_options',
-				array(
-					$this,
-					'admin'
-				),
-				'none', 60 );
+				'inspiro_admin',      // menu slug, todo:check here if ok
+				array( $this, 'admin' ),        // callback function
+				'none',                 // icon (default: none)
+				59                      // position
+			);
 
+			// Add the first submenu page
+//			add_submenu_page(
+//				'inspiro_admin',          // parent slug
+//				__( 'Inspiro Options', 'inspiro' ),  // page title
+//				__( 'Theme Options', 'wpzoom' ),     // menu title
+//				'manage_options',           // capability
+//				'inspiro_options',         // menu slug
+//				array( $this, 'admin' )              // callback function
+//			);
+
+			// Add the "About Inspiro" submenu page
 			add_submenu_page(
-				'wpzoom_options',
-				__( 'Inspiro', 'inspiro' ),
-				__( 'Theme Options', 'wpzoom' ),
-				'manage_options',
-				'inspiro_options', array(
-				$this,
-				'admin'
-			) );
+				'inspiro_admin',         // parent slug
+				__( 'About Inspiro', 'inspiro' ),   // page title
+				__( 'About', 'inspiro' ),           // menu title
+				'manage_options',          // capability
+				'inspiro_about',          // menu slug
+				array( $this, 'about' )             // callback function
+			);
+
+			// Add the "Customizer" submenu page
+			add_submenu_page(
+				'inspiro_admin',         // parent slug
+				__( 'Customizer', 'inspiro' ),      // page title
+				__( 'Customizer', 'inspiro' ),      // menu title
+				'manage_options',          // capability
+				'customize.php',          // menu slug
+			);
+
+			// Add the "Upgrade" submenu page
+			add_submenu_page(
+				'inspiro_admin',         // parent slug
+				__( 'Upgrade', 'inspiro' ),         // page title
+				__( 'Upgrade', 'inspiro' ),         // menu title
+				'manage_options',          // capability
+				'inspiro_upgrade',        // menu slug
+				array( $this, 'upgrade' )           // callback function
+			);
 		}
 	}
 
