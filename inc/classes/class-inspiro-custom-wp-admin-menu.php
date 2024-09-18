d3986befdb5e1a942cf611a8e77b19c17914a5aa
Josanu Andrei <andrei@quadra.md>
2024-09-18 10:31:18 +0300

    Improve readability

--- inc/classes/class-inspiro-custom-wp-admin-menu.php
+++ inc/classes/class-inspiro-custom-wp-admin-menu.php
@@ -30,6 +30,12 @@ if ( ! class_exists( 'Inspiro_Custom_Wp_Admin_Menu' ) ) {
 			require_once INSPIRO_THEME_DIR . 'inc/admin/pages/admin.php';
 		}
 
+		public function call_demo_import_plugin_page() {
+			if ( class_exists( 'OCDI_Plugin' ) ) {
+				$one_click_demo_import = OCDI\OneClickDemoImport::get_instance();
+				$one_click_demo_import->display_plugin_page();
+			}
+		}
 
 		/**
 		 * Register custom menu for wp-admin.
@@ -40,53 +46,41 @@ if ( ! class_exists( 'Inspiro_Custom_Wp_Admin_Menu' ) ) {
 			// Add the main menu page
 			add_menu_page( // phpcs:ignore WPThemeReview.PluginTerritory.NoAddAdminPages.add_menu_pages_add_menu_page
 				__( 'Inspiro Lite Theme Dashboard', 'inspiro' ),   // page title
-				__( 'Inspiro', 'inspiro' ),                         // menu title
-				'manage_options',
-				'inspiro',      // menu slug, todo:check here if ok
-				array( $this, 'admin' ),        // callback function
+				__( 'Inspiro', 'inspiro' ),                        // menu title
+				'manage_options',                         // capability
+				'inspiro',                               // menu slug, todo:check here if ok
+				array( $this, 'admin' ),                           // callback function
 				'data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iNTUwIiBoZWlnaHQ9IjU1MCIgdmlld0JveD0iMCAwIDU1MCA1NTAiIGZpbGw9Im5vbmUiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+CjxwYXRoIGZpbGwtcnVsZT0iZXZlbm9kZCIgY2xpcC1ydWxlPSJldmVub2RkIiBkPSJNMjc1IDU1MEM0MjYuODc4IDU1MCA1NTAgNDI2Ljg3OCA1NTAgMjc1QzU1MCAxMjMuMTIyIDQyNi44NzggMCAyNzUgMEMxMjMuMTIyIDAgMCAxMjMuMTIyIDAgMjc1QzAgNDI2Ljg3OCAxMjMuMTIyIDU1MCAyNzUgNTUwWk0yMzIgMTIzLjMzM0MyMzIgOTkuNDAwOSAyNTEuNDAxIDgwIDI3NS4zMzMgODBIMzE0LjMzM0MzMTYuNzI3IDgwIDMxOC42NjcgODEuOTM5OSAzMTguNjY3IDg0LjMzMzVWMTYxLjQ0M0MzMTguNjY3IDE2Mi44NzIgMzE3LjkwNCAxNjQuMTkyIDMxNi42NjcgMTY0LjkwN0wyMzggMjEwLjMyNUMyMzUuMzMzIDIxMS44NjUgMjMyIDIwOS45NCAyMzIgMjA2Ljg2MVYxMjMuMzMzWk0yMzQgMjg3LjY5TDMxMi42NjcgMjQyLjI3MkMzMTUuMzMzIDI0MC43MzIgMzE4LjY2NyAyNDIuNjU3IDMxOC42NjcgMjQ1LjczNlY0MjYuNjY3QzMxOC42NjcgNDUwLjU5OSAyOTkuMjY2IDQ3MCAyNzUuMzMzIDQ3MEgyMzYuMzMzQzIzMy45NCA0NzAgMjMyIDQ2OC4wNiAyMzIgNDY1LjY2N1YyOTEuMTU0QzIzMiAyODkuNzI1IDIzMi43NjIgMjg4LjQwNCAyMzQgMjg3LjY5WiIgZmlsbD0iYmxhY2siLz4KPC9zdmc+',                 // icon (default: none)
-				59                      // position
+				59                                          // position
 			);
 
 			// Add Dashboard submenu. Same slug as parent to allow renaming the automatic submenu that is added.
 			add_submenu_page( // phpcs:ignore WPThemeReview.PluginTerritory.NoAddAdminPages.add_menu_pages_add_submenu_page
-				'inspiro',                       // parent slug
-				__( 'Inspiro Lite Theme Dashboard', 'inspiro' ),  // page title
-				__( 'Theme Dashboard', 'inspiro' ),                 // menu title
-				'manage_options',                        // capability
-				'inspiro',                        // menu slug
-				array( $this, 'admin' )                           // callback function
+				'inspiro',                              // parent slug
+				__( 'Inspiro Lite Theme Dashboard', 'inspiro' ),   // page title
+				__( 'Theme Dashboard', 'inspiro' ),                // menu title
+				'manage_options',                         // capability
+				'inspiro',                               // menu slug
+				array( $this, 'admin' )                            // callback function
 			);
-
-			// Add the "About Inspiro" submenu page
-//			add_submenu_page( // phpcs:ignore WPThemeReview.PluginTerritory.NoAddAdminPages.add_menu_pages_add_submenu_page
-//				'inspiro_admin',         // parent slug
-//				__( 'About Inspiro', 'inspiro' ),   // page title
-//				__( 'About', 'inspiro' ),           // menu title
-//				'manage_options',          // capability
-//			'admin.php?page=inspiro',    // link was included without a callback render func.
-////				'inspiro_about',                    // menu slug
-////				array( $this,'about' )              // callback function
-//			);
 
 			// Add the "Customizer" submenu page
 			add_submenu_page( // phpcs:ignore WPThemeReview.PluginTerritory.NoAddAdminPages.add_menu_pages_add_submenu_page
 				'inspiro',         // parent slug
-				__( 'Customize', 'inspiro' ),      // page title
-				__( 'Customize', 'inspiro' ),      // menu title
-				'manage_options',          // capability
-				'customize.php',          // menu slug, link was included without a callback render func.
+				__( 'Customize', 'inspiro' ), // page title
+				__( 'Customize', 'inspiro' ), // menu title
+				'manage_options',    // capability
+				'customize.php',    // menu slug, link was included without a callback render func.
 			);
 
 			// Add the "Import Demo" submenu page
 			add_submenu_page( // phpcs:ignore WPThemeReview.PluginTerritory.NoAddAdminPages.add_menu_pages_add_submenu_page
-				'inspiro',         // parent slug
-				__( 'Import Demo', 'inspiro' ),         // page title
-				__( 'Import Demo', 'inspiro' ),         // menu title
-				'manage_options',          // capability
-				''
-//				'inspiro_upgrade',                  // menu slug
-//				array( $this, 'upgrade' )           // callback function
+				'inspiro',            // parent slug
+				__( 'Import Demo', 'inspiro' ),  // page title
+				__( 'Import Demo', 'inspiro' ),  // menu title
+				'manage_options',       // capability
+				'demo-import-demo',    // menu slug,
+				array( $this, 'call_demo_import_plugin_page' )  // callback function
 			);
 
 			// Add the "Upgrade" submenu page
@@ -94,10 +88,8 @@ if ( ! class_exists( 'Inspiro_Custom_Wp_Admin_Menu' ) ) {
 				'inspiro',         // parent slug
 				__( 'Upgrade to Premium', 'inspiro' ),         // page title
 				__( 'Upgrade to Premium', 'inspiro' ),         // menu title
-				'manage_options',          // capability
-				''
-//				'inspiro_upgrade',                  // menu slug
-//				array( $this, 'upgrade' )           // callback function
+				'manage_options',                     // capability
+				''                                   // menu slug,
 			);
 
 			// The "Install Plugins" submenu page was added true class-tgm-plugin-activation.php add_admin_menu( array $args )
