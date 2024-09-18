846f0a2ffc7cad9cb1512a710c441f8e642c4754
Josanu Andrei <andrei@quadra.md>
2024-09-18 10:52:23 +0300

    Improve maintainability.

--- inc/classes/class-inspiro-custom-wp-admin-menu.php
+++ inc/classes/class-inspiro-custom-wp-admin-menu.php
@@ -15,15 +15,16 @@ if ( ! defined( 'ABSPATH' ) ) {
  * Main PHP class for wp-admin options page
  *
  * Menu items:
- *  - About Inspiro
- *  - Customizer
+ *  - Dashboard
+ *  - Customize
+ *  - Import Demo
  *  - Upgrade
  */
-if ( ! class_exists( 'Inspiro_Custom_Wp_Admin_Menu' ) ) {
-	class Inspiro_Custom_Wp_Admin_Menu {
+if ( ! class_exists( 'Inspiro_WP_Admin_Menu' ) ) {
+	class Inspiro_WP_Admin_Menu {
 
 		public function __construct() {
-			add_action( 'admin_menu', array( $this, 'register_admin_pages' ) );
+			add_action( 'admin_menu', array( $this, 'register_menus' ) );
 		}
 
 		public function admin() {
@@ -42,16 +43,27 @@ if ( ! class_exists( 'Inspiro_Custom_Wp_Admin_Menu' ) ) {
 		 *
 		 * @return void
 		 */
-		public function register_admin_pages() {
+		public function register_menus() {
+			$icon = 'data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iNTUwIiBoZWlnaHQ9IjU1MCIgdmlld0JveD0iMCAwIDU1MCA1NTAiI
+				GZpbGw9Im5vbmUiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+CjxwYXRoIGZpbGwtcnVsZT0iZXZlbm9kZCIgY2xpc
+				C1ydWxlPSJldmVub2RkIiBkPSJNMjc1IDU1MEM0MjYuODc4IDU1MCA1NTAgNDI2Ljg3OCA1NTAgMjc1QzU1MCAxMjMuMTIyIDQyNi44N
+				zggMCAyNzUgMEMxMjMuMTIyIDAgMCAxMjMuMTIyIDAgMjc1QzAgNDI2Ljg3OCAxMjMuMTIyIDU1MCAyNzUgNTUwWk0yMzIgMTIzLjMzM0
+				MyMzIgOTkuNDAwOSAyNTEuNDAxIDgwIDI3NS4zMzMgODBIMzE0LjMzM0MzMTYuNzI3IDgwIDMxOC42NjcgODEuOTM5OSAzMTguNjY3IDg
+				0LjMzMzVWMTYxLjQ0M0MzMTguNjY3IDE2Mi44NzIgMzE3LjkwNCAxNjQuMTkyIDMxNi42NjcgMTY0LjkwN0wyMzggMjEwLjMyNUMyMzU
+				uMzMzIDIxMS44NjUgMjMyIDIwOS45NCAyMzIgMjA2Ljg2MVYxMjMuMzMzWk0yMzQgMjg3LjY5TDMxMi42NjcgMjQyLjI3MkMzMTUuMzM
+				zIDI0MC43MzIgMzE4LjY2NyAyNDIuNjU3IDMxOC42NjcgMjQ1LjczNlY0MjYuNjY3QzMxOC42NjcgNDUwLjU5OSAyOTkuMjY2IDQ3MCA
+				yNzUuMzMzIDQ3MEgyMzYuMzMzQzIzMy45NCA0NzAgMjMyIDQ2OC4wNiAyMzIgNDY1LjY2N1YyOTEuMTU0QzIzMiAyODkuNzI1IDIzMi4
+				3NjIgMjg4LjQwNCAyMzQgMjg3LjY5WiIgZmlsbD0iYmxhY2siLz4KPC9zdmc+';
+
 			// Add the main menu page
 			add_menu_page( // phpcs:ignore WPThemeReview.PluginTerritory.NoAddAdminPages.add_menu_pages_add_menu_page
 				__( 'Inspiro Lite Theme Dashboard', 'inspiro' ),   // page title
 				__( 'Inspiro', 'inspiro' ),                        // menu title
 				'manage_options',                         // capability
-				'inspiro',                               // menu slug, todo:check here if ok
+				'inspiro',                               // menu slug
 				array( $this, 'admin' ),                           // callback function
-				'data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iNTUwIiBoZWlnaHQ9IjU1MCIgdmlld0JveD0iMCAwIDU1MCA1NTAiIGZpbGw9Im5vbmUiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+CjxwYXRoIGZpbGwtcnVsZT0iZXZlbm9kZCIgY2xpcC1ydWxlPSJldmVub2RkIiBkPSJNMjc1IDU1MEM0MjYuODc4IDU1MCA1NTAgNDI2Ljg3OCA1NTAgMjc1QzU1MCAxMjMuMTIyIDQyNi44NzggMCAyNzUgMEMxMjMuMTIyIDAgMCAxMjMuMTIyIDAgMjc1QzAgNDI2Ljg3OCAxMjMuMTIyIDU1MCAyNzUgNTUwWk0yMzIgMTIzLjMzM0MyMzIgOTkuNDAwOSAyNTEuNDAxIDgwIDI3NS4zMzMgODBIMzE0LjMzM0MzMTYuNzI3IDgwIDMxOC42NjcgODEuOTM5OSAzMTguNjY3IDg0LjMzMzVWMTYxLjQ0M0MzMTguNjY3IDE2Mi44NzIgMzE3LjkwNCAxNjQuMTkyIDMxNi42NjcgMTY0LjkwN0wyMzggMjEwLjMyNUMyMzUuMzMzIDIxMS44NjUgMjMyIDIwOS45NCAyMzIgMjA2Ljg2MVYxMjMuMzMzWk0yMzQgMjg3LjY5TDMxMi42NjcgMjQyLjI3MkMzMTUuMzMzIDI0MC43MzIgMzE4LjY2NyAyNDIuNjU3IDMxOC42NjcgMjQ1LjczNlY0MjYuNjY3QzMxOC42NjcgNDUwLjU5OSAyOTkuMjY2IDQ3MCAyNzUuMzMzIDQ3MEgyMzYuMzMzQzIzMy45NCA0NzAgMjMyIDQ2OC4wNiAyMzIgNDY1LjY2N1YyOTEuMTU0QzIzMiAyODkuNzI1IDIzMi43NjIgMjg4LjQwNCAyMzQgMjg3LjY5WiIgZmlsbD0iYmxhY2siLz4KPC9zdmc+',                 // icon (default: none)
-				59                                          // position
+				$icon,                                             // icon (default: none)
+				59                                         // position
 			);
 
 			// Add Dashboard submenu. Same slug as parent to allow renaming the automatic submenu that is added.
@@ -64,13 +76,13 @@ if ( ! class_exists( 'Inspiro_Custom_Wp_Admin_Menu' ) ) {
 				array( $this, 'admin' )                            // callback function
 			);
 
-			// Add the "Customizer" submenu page
+			// Add the "Customize" submenu page
 			add_submenu_page( // phpcs:ignore WPThemeReview.PluginTerritory.NoAddAdminPages.add_menu_pages_add_submenu_page
-				'inspiro',         // parent slug
-				__( 'Customize', 'inspiro' ), // page title
-				__( 'Customize', 'inspiro' ), // menu title
-				'manage_options',    // capability
-				'customize.php',    // menu slug, link was included without a callback render func.
+				'inspiro',          // parent slug
+				__( 'Customize', 'inspiro' ),  // page title
+				__( 'Customize', 'inspiro' ),  // menu title
+				'manage_options',     // capability
+				'customize.php',     // menu slug, link was included without a callback render func.
 			);
 
 			// Add the "Import Demo" submenu page
@@ -85,16 +97,16 @@ if ( ! class_exists( 'Inspiro_Custom_Wp_Admin_Menu' ) ) {
 
 			// Add the "Upgrade" submenu page
 			add_submenu_page( // phpcs:ignore WPThemeReview.PluginTerritory.NoAddAdminPages.add_menu_pages_add_submenu_page
-				'inspiro',         // parent slug
-				__( 'Upgrade to Premium', 'inspiro' ),         // page title
-				__( 'Upgrade to Premium', 'inspiro' ),         // menu title
-				'manage_options',                     // capability
-				''                                   // menu slug,
+				'inspiro',                   // parent slug
+				__( 'Upgrade to Premium', 'inspiro' ),  // page title
+				__( 'Upgrade to Premium', 'inspiro' ),  // menu title
+				'manage_options',              // capability
+				''                            // menu slug,
 			);
 
 			// The "Install Plugins" submenu page was added true class-tgm-plugin-activation.php add_admin_menu( array $args )
 		}
 	}
 
-	new Inspiro_Custom_Wp_Admin_Menu();
+	new Inspiro_WP_Admin_Menu();
 }
