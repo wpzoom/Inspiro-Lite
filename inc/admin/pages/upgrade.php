7da436a4b6048a57c63c4ff9dd5389f794cc1d6c
Pavel Ciorici <cioricip@gmail.com>
2024-09-24 16:04:27 +0200

    fix localization

--- inc/admin/pages/upgrade.php
+++ inc/admin/pages/upgrade.php
@@ -79,11 +79,11 @@
                                 <label class="screen-reader-text" for="themezip">
                                     <?php
                                     /* translators: Hidden accessibility text. */
-                                    _e( 'Theme zip file' );
+                                    _e( 'Theme zip file', 'wpzoom' );
                                     ?>
                                 </label>
                                 <input type="file" id="themezip" name="themezip" accept=".zip" />
-                                <?php submit_button( _x( 'Install Now', 'inspiro' ), '', 'install-theme-submit', false ); ?>
+                                <?php submit_button( _x( 'Install Now', 'theme', 'wpzoom' ), '', 'install-theme-submit', false ); ?>
                             </form>
 
                         </div>
