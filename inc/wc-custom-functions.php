0deaa650c6513ce2003da1882e2e220f157e4470
Andrew <thrueword@gmail.com>
2024-10-31 17:35:37 +0200

    Fix first default no value.

--- inc/wc-custom-functions.php
+++ inc/wc-custom-functions.php
@@ -14,7 +14,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 /**
  * Disable WooCommerce Product Zoom
  */
-$get_wc_custom_product_image_zoom_option_value =  get_theme_mod('inspiro_wc_custom_product_image_zoom_option');
+$get_wc_custom_product_image_zoom_option_value =  get_theme_mod('inspiro_wc_custom_product_image_zoom_option', true);
 if ( $get_wc_custom_product_image_zoom_option_value === false ) {
 	remove_theme_support( 'wc-product-gallery-zoom' );
 }
