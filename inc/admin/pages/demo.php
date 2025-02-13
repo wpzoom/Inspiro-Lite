a3cbb1947fe1ae8410bdb575648a5a85d32e3fa2
Alexus450 <Samarschi@gmail.com>
2025-02-13 11:27:15 +0200

    Update demo.php

--- inc/admin/pages/demo.php
+++ inc/admin/pages/demo.php
@@ -4,9 +4,6 @@
 
 ?>
 
-<?php if ( class_exists( 'Inspiro_Starter_Sites_Importer' ) ) : ?>	
-	<?php do_action( 'inspiro_starter_sites_admin_page' ); ?>	
-<?php else : ?>
 	<div class="wpz-onboard_wrapper">
     <div class="wpz_tabs_pages"><!-- #tabs -->
 
@@ -17,31 +14,37 @@
 				<div class="wpz-onboard_content-main">
                     <div id="license" class="wpz-onboard_content-side plugins">
                         <div class="wpz-onboard_content-side-section">
-                            <h3 class="wpz-onboard_content-side-section-title icon-docs">
-                            <?php esc_html_e( 'Inspiro Starter Sites', 'inspiro' ); ?></h3>
+                            <h3 class="wpz-onboard_content-side-section-title icon-docs"><?php esc_html_e( 'Inspiro Starter Sites', 'inspiro' ); ?></h3> 
+							<p class="wpz-onboard_content-main-intro">
+								<?php esc_html_e( 'Importing demo data is the fastest and easiest way to set up your new theme. Choose your desired template, click on \'Import Demo\' and start editing pre-designed content and layouts instead of building everything from scratch.', 'inspiro-starter-sites' ); ?>
+							</p>
                         </div>
                         
                         <div class="theme-info-wrap">
                             <div class="wpz-onboard_content-side-section install-inspiro-starter-sites">
-								<p class="about">
-									<?php esc_html_e( 'Click the button below to install the Inspiro Starter Sites plugin to use this feature.', 'inspiro' ); ?>
-								</p>
-								<p class="section_footer">
-									<button class="button button-primary" id="install-inspiro-starter-sites-btn">
-										<span id="install-inspiro-starter-sites-btn-text"
-												data-installing-text="<?php esc_attr_e( 'Installing Inspiro Starter Sites', 'inspiro' ); ?>"
-										>
-											<?php esc_html_e( 'Install Inspiro Starter Sites', 'inspiro' ); ?>
-										</span>
-										<i class="dashicons dashicons-update spin-icon hidden-element"></i>
-									</button>
+								<?php if ( class_exists( 'Inspiro_Starter_Sites_Importer' ) ) : ?>	
+									<?php do_action( 'inspiro_starter_sites_admin_page' ); ?>
+								<?php else : ?>
+									<p class="about">
+										<?php esc_html_e( 'Click the button below to install the Inspiro Starter Sites plugin to use this feature.', 'inspiro' ); ?>
+									</p>
+									<p class="section_footer">
+										<button class="button button-primary" id="install-inspiro-starter-sites-btn">
+											<span id="install-inspiro-starter-sites-btn-text"
+													data-installing-text="<?php esc_attr_e( 'Installing Inspiro Starter Sites', 'inspiro' ); ?>"
+											>
+												<?php esc_html_e( 'Install Inspiro Starter Sites', 'inspiro' ); ?>
+											</span>
+											<i class="dashicons dashicons-update spin-icon hidden-element"></i>
+										</button>
 
-									<a href="<?php echo esc_url( __( 'https://www.wpzoom.com/documentation/inspiro-lite/inspiro-lite-importing-the-demo-content/', 'inspiro' ) ); ?>"
-										target="_blank"
-										class="button button-secondary" >
-										<?php esc_html_e( 'How it works? &rarr;', 'inspiro' ); ?>
-									</a>
-								</p>
+										<a href="<?php echo esc_url( __( 'https://www.wpzoom.com/documentation/inspiro-lite/inspiro-lite-importing-the-demo-content/', 'inspiro' ) ); ?>"
+											target="_blank"
+											class="button button-secondary" >
+											<?php esc_html_e( 'How it works? &rarr;', 'inspiro' ); ?>
+										</a>
+									</p>
+								<?php endif; ?>	
                             </div>
                         </div>
                     </div>
@@ -53,5 +56,3 @@
 	<?php get_template_part( 'inc/admin/pages/footer' ); ?>
 
 </div>
-
-<?php endif; ?>	
